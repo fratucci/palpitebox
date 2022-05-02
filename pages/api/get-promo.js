3b6a41fca17a244d7b0003aa7e3d408c7ff52d7f
Thiago Fratucci de Matos <thi_fm@yahoo.com.br>
2022-05-02 17:00:45 -0300

    Correção private key

--- pages/api/get-promo.js
+++ pages/api/get-promo.js
@@ -1,12 +1,18 @@
 import { GoogleSpreadsheet } from 'google-spreadsheet'
 const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
 
+const fromBase64 = value => {
+  const buff = new Buffer.from(value, 'base64');
+  return buff.toString('ascii');
+}
+
+
 export default async (req, res) => {
 
   try {
     await doc.useServiceAccountAuth({
       client_email: process.env.SHEET_CLIENT_EMAIL,
-      private_key: process.env.SHEET_PRIVATE_KEY
+      private_key: fromBase64(process.env.SHEET_PRIVATE_KEY)
     })
 
     await doc.loadInfo()
@@ -22,7 +28,7 @@ export default async (req, res) => {
       message: textoCell.value
     }))
 
-    
+
   } catch (err) {
     res.end(JSON.stringify({
       showCoupon: false,
